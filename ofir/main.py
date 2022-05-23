bd8a1ab2f2cec7df1bf694901890356a567d51a7
ofirtamir1 <106065255+ofirtamir1@users.noreply.github.com>
2022-05-23 16:49:07 +0300

    pandas ques

--- ofir/main.py
+++ ofir/main.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
 #ex1
-   df = pd.read_excel("people.xlsx")
+df = pd.read_excel("people.xlsx")
 #ex2
- df2=['age'].mean())
+df2=['age'].mean())
 #ex3
 (df3['name'].count())
 #ex4
 (df4['age'].sum())
 #ex5
 df2 =df3.loc[(df3['gender'] == 'M')]
-df4 =df2.groupby('gender')
+df5 =df2.groupby('gender')
 #df6
-df1 =df.loc[(df['age'] >20)]
-
+df6 =df.loc[(df['age'] >20)]
+#df7
+df7 =df.loc[(df['age'] > 35) | (df['age'] <17)]
+print(df7[['age']])
 
 
