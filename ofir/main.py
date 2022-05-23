7551460faf9377425a06b5226f3a4336a0f9df76
ofirtamir1 <106065255+ofirtamir1@users.noreply.github.com>
2022-05-23 16:37:38 +0300

    removing prints

--- ofir/main.py
+++ ofir/main.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
-df3 = pd.read_excel("people.xlsx")
+#ex1
+   df = pd.read_excel("people.xlsx")
+#ex2
+ df2=['age'].mean())
+#ex3
+(df3['name'].count())
+#ex4
+(df4['age'].sum())
+#ex5
 df2 =df3.loc[(df3['gender'] == 'M')]
 df4 =df2.groupby('gender')
-print(df4['gender'])
+#df6
+df1 =df.loc[(df['age'] >20)]
+
+
 
