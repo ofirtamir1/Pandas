08ed4481b233eed7c3d33d0c8105de3b0f32a82e
ofirtamir1 <106065255+ofirtamir1@users.noreply.github.com>
2022-05-24 16:41:23 +0300

    questions1-19

--- ofir/pandassql.py
+++ ofir/pandassql.py
@@ -5,5 +5,9 @@ df_animals=pd.read_excel('animals.xlsx')
 df1 = df.rename(columns={'name' : 'person_name' , 'age' :'person_age'})
 #ex2
 df2 = df_animals.rename(columns={'ID': 'animalID' , 'Name': 'Animals_name' , 'Age': 'Animalage'})
-print(df2)
 #ex3
+df3 = df.merge (df_animals , left_on= 'name' , right_on = 'Name')
+#print(df3)
+#ex4
+df4= df.merge (df_animals , left_on= 'name' , right_on = 'Name', how= 'left')
+#print(df4)
