47d6de49d24e85b7aab9b0bd8dbdb2b66b6289eb
ofirtamir1 <106065255+ofirtamir1@users.noreply.github.com>
2022-05-24 16:27:23 +0300

    questions pandas animals 9

--- ofir/pandassql.py
+++ ofir/pandassql.py
@@ -1,4 +1,9 @@
 import pandas as pd
-
+#ex1
 df=pd.read_excel('people.xlsx')
 df_animals=pd.read_excel('animals.xlsx')
+df1 = df.rename(columns={'name' : 'person_name' , 'age' :'person_age'})
+#ex2
+df2 = df_animals.rename(columns={'ID': 'animalID' , 'Name': 'Animals_name' , 'Age': 'Animalage'})
+print(df2)
+#ex3
